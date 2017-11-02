b5d3000b088bed5462e70138bc2e92d454f54fb3
je565 <joshuaebyson@yahoo.com>
2017-11-02 08:49:34 -0400

    Initial commit

---
+++ src/com/company/HighSchoolClass.java
@@ -0,0 +1,28 @@
+package com.company;
+
+/**
+ * Created by je565 on 11/2/17.
+ */
+public class HighSchoolClass {
+    private Student [] students;
+
+    public Student getValedictorian()
+    {
+
+    }
+
+    public double getHonorsPercent()
+    {
+
+    }
+
+    public double getGPA()
+    {
+
+    }
+
+    public boolean isHonors)_
+    {
+        
+    }
+}
