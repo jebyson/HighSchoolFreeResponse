39a6e4a22ba312ac22378e9b5998d651cd1c3999
je565 <joshuaebyson@yahoo.com>
2017-11-03 08:15:21 -0400

    Initial Commit

--- src/com/company/HighSchoolClass.java
+++ src/com/company/HighSchoolClass.java
@@ -8,12 +8,40 @@ public class HighSchoolClass {
 
     public Student getValedictorian()
     {
+        Student val;
 
+        for(int i = 0; i < students.length; i++)
+        {
+            int a = students[i].getGPA;
+            int b = students[i + 1].getGPA;
+
+            if(a > b)
+            {
+                val = students[i];
+            }
+            else
+            {
+                val = students[i + 1];
+            }
+        }
+
+        return val;
     }
 
     public double getHonorsPercent()
     {
+        int cnt = 0;
 
+        for(int k = 0; k < students.length; k++)
+        {
+            if(students[k].isHonors)
+            {
+                cnt++;
+            }
+        }
+
+        double perc = (cnt/students.length) * 100;
+        return perc;
     }
 
     public double getGPA()
@@ -21,8 +49,8 @@ public class HighSchoolClass {
 
     }
 
-    public boolean isHonors)_
+    public boolean isHonors()
     {
-        
+
     }
 }
